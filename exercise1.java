8ed1eb39804e913c39ad5ce72b859ee98c8a5e13
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2026-04-19 22:14:42 -0400

    exercise 1:
    added publisher class to book class

---
+++ exercise1.java
@@ -0,0 +1,43 @@
+class Book {
+    String title;
+    Author author;
+    int price;
+    Publisher publisher;
+
+    Book(String title, Author author, int price, Publisher publisher){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.publisher = publisher;
+    }
+}
+
+class Author{
+    String name;
+    int yob;
+
+    Author(String name, int yob){
+        this.name = name;
+        this.yob = yob;
+    }
+}
+
+class Publisher{
+    String name;
+    String country;
+    int established;
+
+    Publisher(String name, String country, int established){
+        this.name = name;
+        this.country = country;
+        this.established = established;
+    }
+}
+
+class ExampleBooks{
+    ExampleBooks(){}
+
+    Author john = new Author("John", 1950);
+    Publisher penguin = new Publisher("Penguin", "Somalia", 1492);
+    Book example = new Book("Example", this.john, 20, this.penguin);
+}
