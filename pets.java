fc197cbcd8c0e619a72a93f560865fd54019aaab
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2026-04-25 22:10:17 -0400

    lab 1 completed.

--- pets.java
+++ pets.java
@@ -15,10 +15,14 @@ import tester.*;
         boolean isOlder(Person other){
             return other.age < this.age;
         }
+
+        void perish(){this.pet = new NoPet();}
+
+        boolean sameNamePet(String name){return this.pet.name() == name;}
     }
 
 
-    interface IPet { }
+    interface IPet { public String name(); }
      
 
     class Cat implements IPet {
@@ -31,6 +35,8 @@ import tester.*;
             this.kind = kind;
             this.longhaired = longhaired;
         }
+
+        public String name(){return this.name;}
     }
      
 
@@ -44,18 +50,36 @@ import tester.*;
             this.kind = kind;
             this.male = male;
         }
+        public String name(){return this.name;}
+    }
+
+    class NoPet implements IPet{
+        NoPet(){}
+
+        public String name(){return "no pet";}
     }
 
     class ExamplesPets{
         ExamplesPets(){}
 
-        Person john = new Person("John", null, 20) ;
-        Person barry = new Person("Barry", null, 18) ;
-        Person susie = new Person("Susie", null, 24) ;
-        Person larry = new Person("Larry", null, 27) ;
+        Person john = new Person("John", new Dog("Spot", "Border Collie", false), 20) ;
+        Person barry = new Person("Barry", new Cat("Tom", "Cat", false), 18) ;
+        Person susie = new Person("Susie", new NoPet(), 24) ;
+        Person larry = new Person("Larry", new Dog("Argos", "Hound", true), 27) ;
 
         boolean testIsOlder(Tester t){
             return t.checkExpect(this.john.isOlder(barry), true) &&
                     t.checkExpect(this.barry.isOlder(susie), false);
+        }
+
+        boolean testName(Tester t){
+            return t.checkExpect(this.john.sameNamePet("Spot"), true) &&
+                    t.checkExpect(this.barry.sameNamePet("Tim"), false);
+        }
+
+
+        boolean testPerish(Tester t){
+            john.perish();
+            return t.checkExpect(this.john.sameNamePet("no pet"), true);
         }
     }
